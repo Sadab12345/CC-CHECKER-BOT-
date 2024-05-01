cc2a52477aa31d048ae05d424b0f72d3a504e722
BuddyxBro <168617299+BuddyxBro@users.noreply.github.com>
2024-05-01 18:01:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $username3       = ('@'.$username);
  $info            = json_encode($update, JSON_PRETTY_PRINT); 
 $emojid = '❌';
 $emojil = '✅';
-$owner = '<code>@iam_daxx</code>';
+$owner = '<code>5816878847</code>';
 $cofuid = '1212';
 $cofuid2 = '1212';
 $cofuid3 = '1212';
@@ -79,7 +79,7 @@ $callbackmessage = $update->callback_query->message->text;
 $callbackid = $update->callback_query->id;
 
 $text = $update->message->text;
-$owner = '<code>@CARD3DBOT</code>';
+$owner = '<code>5816878847</code>';
 
 
 
